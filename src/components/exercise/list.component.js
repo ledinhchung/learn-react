b61089b2343f87d9f355e51dd1a6dbffa1414faf
chungle <chungld@pascaliaasia.com>
2020-06-20 17:17:02 +0700

    Create components

---
+++ src/components/exercise/list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class ExerciseList extends Component {
+
+  render() {
+    return (
+      <h1>Hello from ExerciseList component</h1>
+    )
+  }
+}
---
+++ src/components/user/list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component {
+
+  render() {
+    return (
+      <h1>Hello from ExerciseList component</h1>
+    )
+  }
+}
